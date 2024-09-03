2d9dc91639dcfa834a7271332d23a60c6457475a
Sara Vidal <36409243+Saracvidal@users.noreply.github.com>
2024-09-03 15:47:30 +0200

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace pmv_si_2024_2_pe6_t2_g06_gestao_de_salao_servico_agenda
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
