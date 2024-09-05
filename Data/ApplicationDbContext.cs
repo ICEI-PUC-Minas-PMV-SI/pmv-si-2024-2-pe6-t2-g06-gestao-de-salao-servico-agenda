d3c517b0c90d5393c928c4e77b67de0b9ec3df5f
Sara Vidal <36409243+Saracvidal@users.noreply.github.com>
2024-09-05 22:59:21 +0200

    Configuracao inicial DbContext e conexao com banco de dados

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using pmv_si_2024_2_pe6_t2_g06_gestao_de_salao_servico_agenda.Models;
+
+namespace pmv_si_2024_2_pe6_t2_g06_gestao_de_salao_servico_agenda.Data
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+        // Construtor - Recebe DbContextOptions (Injecao de dependencia) e para option para base "classe pai = DbContext"
+        // Abaixo todas as configuracoes do DbContext (Injecao de dependencia)
+        // Execucao dessa configuracao sera na classe "Program.cs"
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {                
+        }
+
+        // Configuracao dos meus objetos - tabelas (entidades) que vao ser criadas no banco de dados
+        public DbSet<Especializacao> Especializacoes { get; set; }
+        public DbSet<Profissional> Profissionais { get; set; }
+        public DbSet<Agendamento> Agendamentos { get; set; }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 
+using Microsoft.EntityFrameworkCore;
+using pmv_si_2024_2_pe6_t2_g06_gestao_de_salao_servico_agenda.Data;
+
 namespace pmv_si_2024_2_pe6_t2_g06_gestao_de_salao_servico_agenda
 {
     public class Program
     {
+        // Aqui damos um stard das configuracoes do projeto
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +14,13 @@ namespace pmv_si_2024_2_pe6_t2_g06_gestao_de_salao_servico_agenda
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            // Configuracoes do banco de dados --
+            builder.Services.AddDbContext<ApplicationDbContext>(options => //options configura tudo abaixo = injecao = DbContext
+                //configurations a serem executadas abaixo - DefaultConnection = appsettings.json
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
+ 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
