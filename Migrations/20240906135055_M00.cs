7df9b5ffa0c6105ba22e9edfaa5eea4e9896a47d
Sara Vidal <36409243+Saracvidal@users.noreply.github.com>
2024-09-06 16:08:06 +0200

    Configuracao do entity framework - // add-migration M00 , update-database - comando para criar a tabela no banc

---
+++ Migrations/20240906135055_M00.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pmv_si_2024_2_pe6_t2_g06_gestao_de_salao_servico_agenda.Migrations
+{
+    /// <inheritdoc />
+    public partial class M00 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Agendamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataAgendamento = table.Column<DateOnly>(type: "date", nullable: false),
+                    HoraAgendamento = table.Column<TimeOnly>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agendamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Especializacoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TipoEspecializacao = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Especializacoes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profissionais",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataNascimento = table.Column<DateOnly>(type: "date", maxLength: 10, nullable: false),
+                    Genero = table.Column<int>(type: "int", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cep = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EspecializacaoId = table.Column<int>(type: "int", nullable: false),
+                    AgendamentoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profissionais", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Profissionais_Agendamentos_AgendamentoId",
+                        column: x => x.AgendamentoId,
+                        principalTable: "Agendamentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Profissionais_Especializacoes_EspecializacaoId",
+                        column: x => x.EspecializacaoId,
+                        principalTable: "Especializacoes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profissionais_AgendamentoId",
+                table: "Profissionais",
+                column: "AgendamentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profissionais_EspecializacaoId",
+                table: "Profissionais",
+                column: "EspecializacaoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Profissionais");
+
+            migrationBuilder.DropTable(
+                name: "Agendamentos");
+
+            migrationBuilder.DropTable(
+                name: "Especializacoes");
+        }
+    }
+}
--- Models/Especializacao.cs
+++ Models/Especializacao.cs
@@ -14,5 +14,10 @@ namespace pmv_si_2024_2_pe6_t2_g06_gestao_de_salao_servico_agenda.Models
 
         // Relacionamento virtual para agendamentos para carregar informacoes do profissional associados a essa especializacao
         public ICollection<Profissional> Profissionais { get; set; }// -n
+
+        //[Column(TypeName="decimal(18,2)")] -- exemplo de configuracao de decimais
+        // add-migration M00 --- para dar um build nos arquivos de migracao para serem usados para criar tabela no banco
+        // Remove-Migration -- remove os arquivos de migracao
+        // update-database - comando para criar a tabela no banco
     }
 }
